e467ec35a8ce42edc3b9566a94e74ad5ccdc1a86
Tharisata Charoenvasnadumrong <tc511@cam.ac.uk>
2022-06-25 17:38:08 +0100

    Fullscreen slider

--- __main__.py
+++
@@ -1,6 +0,0 @@
-# __main__.py - Main module for Linked List algorithm
-# ----------------------------------------------------
-#
-# February 2022, Tempy Charoenvasnadumrong
-#
-# ----------------------------------------------------
--- portfolio_website/README.md
+++
@@ -1,2 +0,0 @@
-# portfolio_website
-My first portfolio website!
